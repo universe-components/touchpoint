5fffe5865b5d2ef6b705eb540df5964e870baa5f
K哥 <wchswchs@gmail.com>
2025-01-13 00:33:52 +0800

    unify to agent

--- touchpoint-lib/src/main/java/com/universe/touchpoint/TouchPointConstants.java
+++ touchpoint-lib/src/main/java/com/universe/touchpoint/TouchPointConstants.java
@@ -4,9 +4,9 @@ public class TouchPointConstants {
 
     public static final String TOUCH_POINT_PKG = "com.universe.touchpoint.touchpoints";
 
-    public static final String TOUCH_POINT_RECEIVERS = "com.universe.touchpoint.receivers";
+    public static final String TOUCH_POINT_RECEIVERS = "com.universe.agent.receivers";
 
-    public static final String TOUCH_POINT_RECEIVER_FILTERS = "com.universe.touchpoint.receiver.filters";
+    public static final String TOUCH_POINT_RECEIVER_FILTERS = "com.universe.agent.receiver.filters";
 
     public static final String AGENT_NAME_PREFIX = "com.universe.agent.name";
 
