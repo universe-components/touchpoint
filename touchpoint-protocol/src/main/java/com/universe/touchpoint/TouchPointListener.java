83493497258b581020122216f9fa2c3710cfec05
K哥 <wchswchs@gmail.com>
2025-01-21 17:04:42 +0800

    update README.md

--- touchpoint-protocol/src/main/java/com/universe/touchpoint/TouchPointListener.java
+++ touchpoint-protocol/src/main/java/com/universe/touchpoint/TouchPointListener.java
@@ -5,6 +5,6 @@ import android.content.Context;
 public interface TouchPointListener<T extends TouchPoint> {
 
     void onReceive(T touchPoint, Context context);
-    String onAction(T touchPoint, Context context);
+    String onAction(T action, Context context);
 
 }
