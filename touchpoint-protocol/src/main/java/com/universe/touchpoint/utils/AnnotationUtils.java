889183a950b2fcc090c09f1ec44b0c4ac35cd287
K哥 <wchswchs@gmail.com>
2025-02-01 23:30:42 +0800

    fix

--- touchpoint-protocol/src/main/java/com/universe/touchpoint/utils/AnnotationUtils.java
+++ touchpoint-protocol/src/main/java/com/universe/touchpoint/utils/AnnotationUtils.java
@@ -3,8 +3,10 @@ package com.universe.touchpoint.utils;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class AnnotationUtils {
 
@@ -75,9 +77,8 @@ public class AnnotationUtils {
                 Object configInstance = configClass.getDeclaredConstructor().newInstance();
 
                 // 获取配置类的所有字段，用于检查是否存在同名变量
-                Map<String, Field> configFields = Map.ofEntries(
-                        configClass.getDeclaredFields().length == 0 ? Map.entry("", null) : null // 解决空字段情况
-                );
+                Map<String, Field> configFields = Arrays.stream(configClass.getDeclaredFields())
+                        .collect(Collectors.toMap(Field::getName, field -> field));
                 for (Field field : configClass.getDeclaredFields()) {
                     field.setAccessible(true);
                     configFields.put(field.getName(), field);
