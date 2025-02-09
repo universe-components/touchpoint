5dca8c3ba15324f5d401275e63435f55bbb3ef92
K哥 <wchswchs@gmail.com>
2025-02-09 23:21:05 +0800

    fix

--- touchpoint-protocol/src/main/java/com/universe/touchpoint/driver/Collaboration.java
+++ touchpoint-protocol/src/main/java/com/universe/touchpoint/driver/Collaboration.java
@@ -7,19 +7,10 @@ import java.util.Map;
 
 public class Collaboration {
 
-    private Class<Enum<?>> statusClass;
     private final Map<String, ActionCoordinator> operatorMap = new HashMap<>();
-
-    public void setStatusClass(Class<Enum<?>> statusClass) {
-        this.statusClass = statusClass;
-    }
 
     public void registerOperator(String status, ActionCoordinator actionCoordinator) {
         operatorMap.put(status, actionCoordinator);
-    }
-
-    public Class<Enum<?>> getStatusClass() {
-        return statusClass;
     }
 
     public ActionCoordinator getOperator(String status) {
