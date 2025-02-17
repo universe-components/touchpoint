0bfb634a86878084e6394a21efb6a70de580bc36
K哥 <wchswchs@gmail.com>
2025-02-17 19:32:58 +0800

    clean code

--- touchpoint-protocol/src/main/java/com/universe/touchpoint/monitor/ActionMonitor.java
+++ touchpoint-protocol/src/main/java/com/universe/touchpoint/monitor/ActionMonitor.java
@@ -17,7 +17,10 @@ public class ActionMonitor<T extends TouchPoint> implements ActionChecker<T, Mon
 
         assert metricConfig != null;
         if (action.getMetric().getPredictionCount().get() > metricConfig.getMaxPredictionCount()) {
-            monitorResult.setState(new DriverState(TaskState.NEED_SWITCH_AI_MODEL.getCode(), "failed", touchPoint.getHeader().getToAction().getActionName()));
+            monitorResult.setState(new DriverState(
+                    TaskState.NEED_SWITCH_AI_MODEL.getCode(),
+                    "The AI model has too many prediction rounds and still hasn't provided a final result",
+                    touchPoint.getHeader().getToAction().getActionName()));
             return monitorResult;
         }
 
