bd54d7786879c5b46673c2c00bcafb30f9e4234d
K哥 <wchswchs@gmail.com>
2025-03-06 21:19:57 +0800

    fix

--- touchpoint-spring/src/test/java/com/universe/touchpoint/spring/TaskMetaTest.java
+++ touchpoint-spring/src/test/java/com/universe/touchpoint/spring/TaskMetaTest.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@SpringBootTest(classes = TaskTestApplication.class)
 public class TaskMetaTest {
 
     @Autowired
---
+++ touchpoint-spring/src/test/java/com/universe/touchpoint/spring/TaskTestApplication.java
@@ -0,0 +1,13 @@
+package com.universe.touchpoint.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskTestApplication.class, args);
+    }
+
+}
