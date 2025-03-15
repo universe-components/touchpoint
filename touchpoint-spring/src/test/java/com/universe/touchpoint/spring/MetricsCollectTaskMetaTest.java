e81b5db30f7426c3e3f290be1a834c181acd358b
K哥 <wchswchs@gmail.com>
2025-03-15 22:04:45 +0800

    Unify action and role lifecycle

--- touchpoint-spring/src/test/java/com/universe/touchpoint/spring/MetricsCollectTaskMetaTest.java
+++ touchpoint-spring/src/test/java/com/universe/touchpoint/spring/MetricsCollectTaskMetaTest.java
@@ -3,7 +3,6 @@ package com.universe.touchpoint.spring;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.universe.touchpoint.meta.data.TaskMeta;
-
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,8 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest(classes = TaskTestApplication.class)
 public class MetricsCollectTaskMetaTest {
 
-  @Autowired
-  private TaskMeta taskMeta;
+  @Autowired private TaskMeta taskMeta;
 
   @Test
   public void testTask() {
